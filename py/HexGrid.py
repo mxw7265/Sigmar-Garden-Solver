16f8c21257202e4e1f1eaca0236975ce99375f08
Micah Wood <micahwood50@gmail.com>
2020-07-31 00:23:03 -0400

    Create HexGrid.py

---
+++ py/HexGrid.py
@@ -0,0 +1,30 @@
+class HexGrid:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.element = None
+
+    def getPosition(self):
+        return self.x, self.y
+
+    def setElement(self, element):
+        self.element = element
+
+    def getElement(self):
+        return self.element
+
+    def removeElement(self):
+        self.element = None
+
+    def getNeighborPositions(self):
+        x = self.x
+        y = self.y
+
+        return (
+            (x-1, y-1),
+            (x  , y-1),
+            (x+1, y  ),
+            (x+1, y+1),
+            (x  , y+1),
+            (x-1, y  )
+        )
